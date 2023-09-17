1c6a030a405aac8e97cd98a7bfcab6e42372c1c8
FreddyPinto <freddypinto@outlook.com>
2023-09-16 20:25:36 -0500

    initial commit

--- Reto_de_Arrays/arrays.js
+++ Reto_de_Arrays/arrays.js
@@ -3,7 +3,7 @@ import dataBase from "./CarDataBase";
 // 1. Obtener el costo total de los carros en inventario
 
 function costoTotal(db) {
-  return 0;
+  return db.reduce((total, carro) => total + carro.cost, 0);
 }
 
 costoTotal(dataBase);
@@ -11,7 +11,14 @@ costoTotal(dataBase);
 // 2. Obtener el promedio de los precios de los carros pares
 
 function promedio(db) {
-  return 0;
+  // Filtrar los carros en posiciones pares
+  const carrosPares = db.filter((carro, index) => index % 2 === 0);
+
+  // Calcular la suma de los precios
+  const sumaPrecios = carrosPares.reduce((total, carro) => total + carro.cost, 0);
+
+  // Calcular el promedio
+  return sumaPrecios / carrosPares.length;
 }
 
 promedio(dataBase);
@@ -19,7 +26,7 @@ promedio(dataBase);
 // ### 3. Obtener los carros Híbridos
 
 function carrosHibridos(db) {
-  return [];
+  return db.filter(carro => carro.fuel_type === "Hybrid");
 }
 
 carrosHibridos(dataBase);
@@ -27,23 +34,45 @@ carrosHibridos(dataBase);
 // ### 4. Obtener el producto más caro
 
 function carroMasCaro(db) {
-  return {};
+  return db.reduce((carroMasCaro, carroActual) => {
+    return (carroActual.cost > carroMasCaro.cost) ? carroActual : carroMasCaro;
+  });
 }
 
 carroMasCaro(dataBase);
 
 // ## 5. Agregar un nuevo producto
 
-function nuevoCarro(db) {
-  return [];
+function nuevoCarro(db, carro) {
+  return [...db, carro];
 }
+
+const carro = {
+  car_name: "Nuevo Carro",
+  cost: 50000,
+  model: "Sedan",
+  manufacturer: "Nuevo Fabricante",
+  color: "Azul",
+  num_doors: 4,
+  cylinder_capacity: 2000,
+  fuel_type: "Gasolina",
+  transmission: "Automático",
+  drive_type: "Tracción delantera",
+  horsepower: 150
+};
 
 nuevoCarro(dataBase);
 
 // ### 6. Eliminar el carro mas económico
 
 function eliminarCarro(db) {
-  return [];
+  // Encontrar el carro más económico
+  const carroMasEconomico = db.reduce((carroMasEconomico, carroActual) => {
+    return (carroActual.cost < carroMasEconomico.cost) ? carroActual : carroMasEconomico;
+  });
+
+  // Crear un nuevo array sin el carro más económico
+  return db.filter(carro => carro !== carroMasEconomico);
 }
 
 eliminarCarro(dataBase);
--- Reto_de_Objects/objects.js
+++ Reto_de_Objects/objects.js
@@ -10,24 +10,44 @@ const tienda = {
     },
   ],
   // 1. Crear un método en el objeto `tienda` llamado `agregarProducto`que permita agregar mas productos al array de productos.
-  agregarProducto: ".. aca resolver el reto ..",
+  agregarProducto: function (producto) {
+    this.productos.push(producto);
+  },
   // 2. Crear un método en el objeto `tienda` llamado `obtenerProducto` que reciba como parámetro el nombre del producto y devuelva el objeto correspondiente
-  obtenerProducto: ".. aca resolver el reto ..",
+  obtenerProducto: function (nombre) {
+    return this.productos.find(producto => producto.nombre === nombre);
+  },
   // 3. Crear un método en el objeto `tienda` llamado `realizarVenta` que reciba como parámetros el nombre del producto y la cantidad vendida, y actualice la cantidad de stock correspondiente.
-  realizarVenta: ".. aca resolver el reto ..",
+  realizarVenta: function (nombre, cantidad) {
+    const producto = this.obtenerProducto(nombre);
+    if (producto && producto.stock >= cantidad) {
+      producto.stock -= cantidad;
+    } else {
+      console.log('Stock insuficiente o producto no encontrado');
+    }
+  },
   // 4. Crear una función llamada `eliminarProducto` que reciba como parámetro el nombre del producto y lo elimine de la lista de productos.
-  eliminarProducto: ".. aca resolver el reto ..",
+  eliminarProducto: function (nombre) {
+    const index = this.productos.findIndex(producto => producto.nombre === nombre);
+    if (index !== -1) {
+      this.productos.splice(index, 1);
+    }
+  },
 };
-
 // 5. Utilizar el método `Object.create` para crear un nuevo objeto llamado `tiendaOnline`
-const tiendaOnline = ".. aca resolver el reto ..";
-
+const tiendaOnline = Object.create(tienda, {
+  online: { value: true, writable: true },
+});
+tiendaOnline.agregarStock = function (nombre, cantidad) {
+  const producto = this.obtenerProducto(nombre);
+  if (producto) {
+    producto.stock += cantidad;
+  }
+};
 // 6. Utilizar el método `Object.keys` para obtener un array con los nombres de las propiedades de los productos de la `tiendaOnline`.
-
-const nombres = ".. aca resolver el reto ..";
+const nombres = Object.keys(tiendaOnline.productos[0]);
 
 // 7. Utilizar el método `Object.values` para obtener un array con todas las cantidades de stock disponibles de la tiendaOnline.
-
-const stock = ".. aca resolver el reto ..";
+const stock = Object.values(tiendaOnline.productos[0]);
 
 export { tienda, tiendaOnline, nombres, stock };
